2bf0085d7053d43a2df05ebab4679886c4892d28
Enrique Velasco <velascoenrique396@gmail.com>
2021-08-14 13:59:52 -0500

    fisrt commi

---
+++ src/components/Navigation.vue
@@ -0,0 +1,110 @@
+<template>
+  <nav>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+      app
+      disable-resize-watcher
+      
+    >
+      <v-app-bar-nav-icon @click="drawer = true" class="d-flex d-sm-flex d-md-none d-lg-none d-xl-none"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+
+    <!-- Navigation Drawer -->
+    <v-navigation-drawer
+        v-model="drawer"
+        class="deep-purple accent-4"
+        dark
+        app
+        bottom
+        >
+        <v-list>
+            <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            >
+            <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+            </v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+            <div class="pa-2">
+            <v-btn block>
+                Logout
+            </v-btn>
+            </div>
+        </template>
+        </v-navigation-drawer>
+  </nav>
+
+  
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: true,
+            items: [
+                { title: 'Home', icon: 'mdi-home-city' },
+                { title: 'My Account', icon: 'mdi-account' },
+                { title: 'Users', icon: 'mdi-account-group-outline' },
+            ],
+            mini: true,
+                
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
